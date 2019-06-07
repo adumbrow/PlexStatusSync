ac41b62126cfbbfb437c28ca50957140fb0487eb
Aaron Dumbrow <adumbrow@Aarons-MBP-2.dumbrows.int>
2019-06-06 17:48:43 -0700

    Testing a direct http request call

--- PlexDirectTest.py
+++ PlexDirectTest.py
@@ -1,4 +1,28 @@
 import requests
 
 url = 'http://172.24.11.48.:32400/?X-Plex-Token=WgQ6JgEQeTShpPYCzgk1'
+data = '''{
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "text": {
+            "record.document": "SOME_JOURNAL"
+          }
+        },
+        {
+          "text": {
+            "record.articleTitle": "farmers"
+          }
+        }
+      ],
+      "must_not": [],
+      "should": []
+    }
+  },
+  "from": 0,
+  "size": 50,
+  "sort": [],
+  "facets": {}
+}'''
 response = requests.post(url, data=data)
